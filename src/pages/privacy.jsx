ff8b200411c27359c08f0915e06da6e8730c20db
Julio Javier Ledezma Yelamo <mycomysticpro@gmail.com>
2025-07-09 19:25:24 -0400

    Fix privacy page for Vite

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
-
 export default function PrivacyPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black px-4">
-      <Image
+      <img
         src="/terms.png"
         alt="Terms and Conditions"
-        width={1200}  // Ajusta si la imagen tiene otro tamaño
-        height={1800} // Ajusta si la imagen tiene otro tamaño
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
       />
     </div>
   );
