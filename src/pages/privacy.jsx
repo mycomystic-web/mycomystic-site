d79b61b99d5c0496528c6246ba375c84121f2710
Julio Javier Ledezma Yelamo <mycomysticpro@gmail.com>
2025-07-09 20:06:51 -0400

    Update Privacy & Terms image

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -2,8 +2,8 @@ export default function PrivacyPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black px-4">
       <img
-        src="/terms.png"
-        alt="Terms and Conditions"
+        src="/privacy-terms-new.png"
+        alt="Privacy & Terms"
         style={{
           maxWidth: "100%",
           height: "auto",
