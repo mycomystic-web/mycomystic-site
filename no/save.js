3961ccd7fc35dced5048b0c54d88f2a184072db6
Julio Javier Ledezma Yelamo <mycomysticpro@gmail.com>
2026-06-08 23:53:45 -0400

    Fix wallet save API

--- no/save.js
+++ no/save.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { address } = req.body;
+  const { wallet: address } = req.body;
 
   if (!address) {
     return res.status(400).json({ error: 'No wallet address provided' });
